0c815665a433098787790ad7e6a5c65476265523
divyanshu930 <divyanshu@myanatomy.in>
2025-03-11 15:00:07 +0530

    Setup for Express, DB(mongodb), Logger(Winston)

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import logger from './logger.js';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info(`DB connection established with ${MONGODB_URI}`);
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+app.get('/', (req, res) => {
+  res.send('opinion trading app');
+});
+
+await connectDB();
+
+app.listen(port, () => {
+  logger.info(`Server started, listening on port ${port}`);
+});
---
+++ src/logger.js
@@ -0,0 +1,18 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.prettyPrint(),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console({
+      handleExceptions: true,
+    }),
+  ],
+  exitOnError: false,
+});
+
+export default logger;
